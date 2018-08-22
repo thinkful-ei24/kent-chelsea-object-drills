eb06425c3687230d12f8089b00aa2f100c3392c0
Kent Tokunaga <kenttokunaga@gmail.com>
2018-08-22 14:01:02 -0400

    Object initializers and methods

--- objectDrills.js
+++ objectDrills.js
@@ -83,5 +83,17 @@ function validateKeys(object, expectedKeys) {
 	return true;
 }
 
+// Advanced Object Drills
+// Object initializers and methods
 
-//
+const loaf = { 
+  flour: 300,
+  water: 210,
+  hydration: function() {
+    return (this.water / this.flour) * 100;
+  }
+}
+
+console.log(loaf.flour);
+console.log(loaf.water);
+console.log(loaf.hydration());
