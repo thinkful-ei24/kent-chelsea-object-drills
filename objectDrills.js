3f166d0af5627b27d937714815739c36a44365b7
clkent <chelsea.kent@gmail.com>
2018-08-22 13:57:41 -0700

    bonus II a database method

--- objectDrills.js
+++ objectDrills.js
@@ -270,4 +270,34 @@ function findOne(arr, query) {
   return result;
 }
 
-//
+// BONUS II: A database method
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  },
+  findOne: function(query) {
+    let result = this.store.heroes.find(heroObject => {
+      for (let i = 0; i < Object.keys(query).length; i++) {
+        if (!Object.keys(heroObject).includes(Object.keys(query)[i])) return false;
+        if (heroObject[Object.keys(query)[i]] !== query[Object.keys(query)[i]]) return false;
+      }
+      return true;
+    });
+  
+    if (result === undefined) result = null;
+    return result;
+  }
+}
+
+
+console.log(Database.findOne({ id: 2 }));
+// => { id: 2, name: 'Iron Man', squad: 'Avengers' }
