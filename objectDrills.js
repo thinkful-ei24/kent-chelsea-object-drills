3ac5d340788d522e9afca2941297594e7e8cbaae
clkent <chelsea.kent@gmail.com>
2018-08-22 08:42:14 -0700

    Object updater drill

--- objectDrills.js
+++ objectDrills.js
@@ -8,3 +8,16 @@ function createMyObject() {
     }
   }
 }
+
+// Object updater
+
+function updateObject(obj) {
+  
+  obj.foo = 'foo'
+  obj.bar = 'bar'
+  obj.bizz = 'bizz'
+  obj.bang = 'bang'
+  
+ return obj;
+}
+
